05b19cae7437ebb4bbf8b493d7762d04720d7a6d
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2023-05-07 04:26:25 +0300

    FIX: update dto with appropriate types

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,5 +1,48 @@
+import { Field, InputType } from '@nestjs/graphql';
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  Matches,
+} from 'class-validator';
+
+@InputType()
 export class CreateUserInput {
-  id: string;
+  @Field()
+  @IsString()
+  @IsNotEmpty()
   first_name: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
   last_name: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/,
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+    },
+  )
+  password: string;
+
+  @Field()
+  @IsString()
+  confirmPassword: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @Field()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
 }
